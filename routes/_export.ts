27a8a9c7718a331a3e6f1edb53a54d5897638e33
Savitoj Cheema <savi@Savitojs-MacBook-Pro.local>
2023-04-24 17:17:00 +0530

    init

---
+++ routes/_export.ts
@@ -0,0 +1,14 @@
+// Exports router modules for serverless env that doesn't support the dynamic import.
+// This module will be updated automaticlly in develoment mode, do NOT edit it manually.
+
+import * as $0 from "./_404.tsx";
+import * as $1 from "./_app.tsx";
+import * as $2 from "./todos.tsx";
+import * as $3 from "./index.jsx";
+
+export default {
+  "/_404": $0,
+  "/_app": $1,
+  "/todos": $2,
+  "/": $3,
+};
